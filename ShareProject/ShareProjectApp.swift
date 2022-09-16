cb5434bae6c29cfabbdc835dd6f43f8d5273e539
Anna <anna.vasi4ko@icloud.com>
2022-09-16 17:58:45 +0300

    Initial Commit

---
+++ ShareProject/ShareProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareProjectApp.swift
+//  ShareProject
+//
+//  Created by Vasichko Anna on 16.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ShareProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
