bac81cd0e9df9da25e6e5b8028ddf9dd05f576d8
Anna <anna.vasi4ko@icloud.com>
2022-09-18 12:00:11 +0300

    share link project

--- ShareProject/ContentView.swift
+++ ShareProject/ContentView.swift
@@ -7,13 +7,48 @@
 
 import SwiftUI
 
+struct ImageToShare: Transferable {
+    
+    static var transferRepresentation: some TransferRepresentation {
+        ProxyRepresentation(exporting: \.image)
+    }
+    
+    var image: Image
+    var title: String
+}
+
 struct ContentView: View {
+    
+    private let url = URL(string: "https://swiftbook.org/")!
+    private let imageToShare = ImageToShare(image: Image("swiftLogo"), title: "Swiftbook.org")
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Image(systemName: "swift")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .foregroundColor(.orange)
+            
+            ShareLink(item: url)
+            
+            ShareLink("Share our link!", item: url)
+            
+            ShareLink(item: url) {
+                Label("Share our website", systemImage: "network")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(.blue, in: RoundedRectangle(cornerRadius: 20))
+            }
+            
+            ShareLink(
+                item: imageToShare,
+                preview: SharePreview(
+                    imageToShare.title,
+                    image: imageToShare.image
+                )
+            ) {
+                Text("Save the logo")
+            }
         }
         .padding()
     }
